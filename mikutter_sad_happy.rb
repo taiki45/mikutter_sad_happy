f4ade95cc14728295c961dc2d5f83c355817daa8
Taiki ONO <taiks.4559@gmail.com>
2013-03-18 02:44:20 +0900

    fix how to create nyan sentence

--- mikutter_sad_happy.rb
+++ mikutter_sad_happy.rb
@@ -8,8 +8,8 @@ Plugin.create :mikutter_sad_happy do
 
     if (0..6).cover? now.hour
       buff = ::Plugin.create(:gtk).widgetof(box).widget_post.buffer
-      if rand(20) == 10
-        buff.text = (texts * rand(0..5)).map {|e| e.sample }.join
+      if rand(20) == 0
+        buff.text = Array.new(rand(1..6)) { texts.sample }.join
       else
         buff.text = buff.text.gsub(/…|\./u, "♡")
       end
